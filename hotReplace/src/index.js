1e85a9bdf7943eeb39d4d0034b15425f078f6b09
蒋敏 <jiangmin@up366.com>
2021-05-17 21:49:01 +0800

    fix:之前绑定事件未做销毁

--- hotReplace/src/index.js
+++ hotReplace/src/index.js
@@ -14,12 +14,17 @@ function component() {
 
     return element;
 }
+let element = component()
 
-document.body.appendChild(component());
+document.body.appendChild(element);
 
 if (module.hot) {
     module.hot.accept('./print.js', function () {
         console.log('Accepting the updated printMe module!');
-        printMe();
+        // printMe();
+        element.removeEventListener('click');
+        document.body.removeChild(element);
+        element = component();
+        document.body.append(element);
     })
 }
