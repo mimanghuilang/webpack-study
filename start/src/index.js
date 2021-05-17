414cd4692141d339b548e29d631b7599e9a68014
蒋敏 <jiangmin@up366.com>
2021-05-17 17:07:55 +0800

    fix: 起步加入loadsh

--- start/src/index.js
+++ start/src/index.js
@@ -1,6 +1,8 @@
+import _ from 'loadsh'
 var foo = function foo() {
-    console.log('foo')
-    return 'foo'
+    var str = _.join(['hello','webpack']);
+    console.log(str);
+    return str
 }
 var haha = document.getElementById('haha');
 haha.addEventListener('click',foo)
