76d1daa8cffabecd883022cc398cfe07143a1e2f
蒋敏 <jiangmin@up366.com>
2021-06-02 17:33:00 +0800

    fix: 初始化提交

---
+++ .eslintrc.js
@@ -0,0 +1,14 @@
+module.exports = {
+  root: true,
+  env: {
+    browser: true,
+    es2021: true
+  },
+  extends: [
+    'standard'
+  ],
+  parserOptions: {
+    ecmaVersion: 12
+  },
+  rules: {}
+}
